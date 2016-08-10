8d6e014ace987b0fc46ac5329ed7e9ab52675ace
Adrian Cunanan <adrian@adriancunanan.com>
2016-08-10 19:34:27 -0400

    Implement Basic Response from api.ai

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,15 @@ class MessagesController < ApplicationController
   def reply
     message_body = params["Body"]
     from_number = params["From"]
+    boot_api_ai
+    response = @ai_client.text_request message_body
+    ai_response = response[:result][:speech]
+
     boot_twilio
     sms = @client.messages.create(
                               from: ENV["twilio_number"],
                               to: from_number,
-                              body: "Hello there, thanks for texting me.  Your number is #{from_number}."
+                              body: ai_response
     )
   end
 
@@ -19,4 +23,14 @@ class MessagesController < ApplicationController
     auth_token = ENV["twilio_token"]
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
+
+  def boot_api_ai
+    @ai_client = ApiAiRuby::Client.new(
+                                      client_access_token: ENV["api_ai_client_access_token"]
+    )
+  end
+
+  @ai_client = ApiAiRuby::Client.new(
+      client_access_token: '5f423ed0ba4f4ef3a427a56185889232'
+  )
 end
