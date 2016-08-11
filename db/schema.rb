271fec292586b28eb3614fe6a1e54cb9ce19c6bd
Adrian Cunanan <adrian@adriancunanan.com>
2016-08-11 16:59:23 -0400

    Add Phone Numbers to Thrivers and set for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811194156) do
+ActiveRecord::Schema.define(version: 20160811203526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20160811194156) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone_number"
+    t.string   "phone_pin"
+    t.boolean  "phone_verified"
   end
 
   add_index "thrivers", ["email"], name: "index_thrivers_on_email", unique: true, using: :btree
